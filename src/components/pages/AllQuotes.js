98ae65761d16ddc15aa87295dcc84148bc49c096
Martin Soengas <martinsoengas@Martins-Macbook-Pro.local>
2022-03-02 18:37:14 -0300

    added quote list funtionality and dummy data

--- src/components/pages/AllQuotes.js
+++ src/components/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from "../quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Martin", text: "Learning react is great!" },
+  { id: "q2", author: "John", text: "Some dummy data" },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
