0a0790b708c6cf695b6acfac13e42d46f1c7dc78
Martin Soengas <martinsoengas@Martins-MBP.fibertel.com.ar>
2022-03-07 13:25:20 -0300

    added sorting of quotes

--- src/components/pages/NewQuote.js
+++ src/components/pages/NewQuote.js
@@ -1,8 +1,13 @@
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+
 import QuoteForm from "../quotes/QuoteForm";
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+    history.push("quotes");
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
