5a06a1b4b3bae70381749ea7669d3790169472b8
Martin Soengas <martinsoengas@Martins-Macbook-Pro.local>
2022-03-07 17:36:44 -0300

    added fetching of quotes

--- src/components/pages/AllQuotes.js
+++ src/components/pages/AllQuotes.js
@@ -1,12 +1,39 @@
 import QuoteList from "../quotes/QuoteList";
-
-const DUMMY_QUOTES = [
-  { id: "q1", author: "Martin", text: "Learning react is great!" },
-  { id: "q2", author: "John", text: "Some dummy data" },
-];
+import useHttp from "../../hooks/use-http";
+import { getAllQuotes } from "../../lib/api";
+import { useEffect } from "react";
+import LoadingSpinner from "../UI/LoadingSpinner";
+import NoQuotesFound from "../quotes/NoQuotesFound";
 
 const AllQuotes = () => {
-  return <QuoteList quotes={DUMMY_QUOTES} />;
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true);
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered focused">{error}</p>;
+  }
+
+  if (status === "completed" && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return <NoQuotesFound />;
+  }
+
+  return <QuoteList quotes={loadedQuotes} />;
 };
 
 export default AllQuotes;
